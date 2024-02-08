39df723bb2ee85829c80dc0e2136806c8cc08f91
Gadañotti Leandro <leoda6@hotmail.com>
2024-02-08 19:01:53 -0300

    primer entrega

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './styles/App.css'
+import { Test } from './components/Test'
+export const App = () => {
+    return (
+        <>
+         <Test/>
+        </>
+    )
+}
+
+
---
+++ src/components/Test.jsx
@@ -0,0 +1,11 @@
+
+
+export const Test = ({mensaje}) => {
+    return (
+        <div>
+            <p>{mensaje}</p>
+        </div>
+    )
+}
+
+
